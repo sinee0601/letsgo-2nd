d04147e68e0de3c1eed8e476d7a0ab558e9c59b4
Seeun <happysarahkim2004@gmail.com>
2026-06-05 16:19:59 +0900

    Config 파일 빠져서 추가

--- src/main/java/com/travel/letsgospringboot/user/auth/SecurityConfig.java
+++ src/main/java/com/travel/letsgospringboot/user/auth/SecurityConfig.java
@@ -23,17 +23,24 @@ public class SecurityConfig {
 
         //비로그인
         http.authorizeHttpRequests(auth ->
+<<<<<<< HEAD
                 auth.requestMatchers("/**").permitAll()
                         .anyRequest().authenticated());
+=======
+                auth.requestMatchers("/", "/user/loginView", "/user/signUpView", "/user/signUp").permitAll()
+                        .requestMatchers("/user/**").authenticated()
+                        .anyRequest().permitAll());
+>>>>>>> UserController
 
         //로그인
         http.formLogin(form
-                        -> form.loginPage("/user/login")
+                        -> form.loginPage("/user/loginView")
                         .loginProcessingUrl("/login")
                         .usernameParameter("userID")
                         .passwordParameter("password")
-                        .failureUrl("/user/login?error=true")
+                        .failureUrl("/user/loginView?error=true")
                         .defaultSuccessUrl("/user/user")
+                        .permitAll()
         );
         return http.build();
     }
